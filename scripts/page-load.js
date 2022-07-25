f763b399df681bd37cbaa29f0b7448cfa3791f44
ozlemyb <109896695+ozlemyb@users.noreply.github.com>
2022-07-25 14:46:01 +0300

    Add files via upload

--- scripts/page-load.js
+++ scripts/page-load.js
@@ -3,4 +3,7 @@
 window.addEventListener('load', function() {
     this.document.body.classList.remove('fade');    
 });
-window.addEventListener('unload', function() {});
+//window.addEventListener('unload', function() {});
+window.addEventListener('pageshow', function() {
+    this.document.body.classList.remove('fade');    
+});
